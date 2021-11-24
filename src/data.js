0690f10693d94257daac92742aaa527878194899
Henrick Tambourou <h.tambourou@gmail.com>
2021-11-24 00:40:33 -0600

    bug fixes

--- src/data.js
+++ src/data.js
@@ -30,7 +30,7 @@ export const projects = [
       },
       {
         title: 'Weather App',
-        description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+        description: "A website that uses a weather api to display weather based on a user's current location.",
         image: img4,
         tags: ['Javascript'],
         source: 'https://github.com/H-Tambourou/Weather',
